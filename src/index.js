c0dd26448fdff48bd3366e97943d944582ecbc06
kbosedbl <kbosedbl@gmail.com>
2020-02-14 12:20:41 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import CardList from './CardList';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import {robots} from './robots';
+
+ReactDOM.render(<App /> , document.getElementById('root'));
+
+
+serviceWorker.unregister();
